76a4d84f09dcd09841966b9e02f5ecc66808f269
Alex Naidis <alex.naidis@linux.com>
2016-10-19 19:05:24 +0200

    robin: correct manifest
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>

--- robin/Android.mk
+++ robin/Android.mk
@@ -1,4 +1,4 @@
-# Copyright (C) 2016 Paranoid Android
+# Copyright (C) 2016 PAxNextbit
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
